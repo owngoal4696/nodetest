fea924d3fd7f2e27b7d133b469bf2f5af5405d45
Toru Sakuma <corposak781@gmail.com>
2024-02-16 18:47:44 +0900

    コミットメッセージ！

---
+++ src/components/NavigationBar.vue
@@ -0,0 +1,46 @@
+<template>
+  <!-- ナビゲーションバーをダークテーマで作成 -->
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <!-- タイトル -->
+    <a class="navbar-brand" href="/main">todoリスト</a>
+
+    <!-- 画面サイズに応じて折りたたむ設定  -->
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <!-- メニュー全体 -->
+    <div class="collapse navbar-collapse" id="navbarText">
+      <ul class="navbar-nav">
+        <!-- ノートリストページへのリンク -->
+        <li class="nav-item">
+          <a class="nav-link" href="/main"><i class="fas fa-list-alt"></i>メイン</a>
+        </li>
+
+        <!-- 学習ページへのリンク -->
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" data-toggle="dropdown">
+            <i class="fas fa-check"></i>GO
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="/study/binding">ページ1</a></li>
+            <li><a class="dropdown-item" href="/study/directive">ページ2</a></li>
+            <li><a class="dropdown-item" href="/study/event">ページ3</a></li>
+            <li><a class="dropdown-item" href="/study/todolist">ページ4</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped>
+.navigation-bar {
+  height: 60px;
+}
+</style>
---
+++ src/components/study/Todolist.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="main-page">
+        <div class="left-menu">
+      <!-- TODOリスト -->
+      <input type="text" v-model="newTodoText" /><button @click="addTodo">追加</button>
+      <button @click="clearDoneTodos">完了済みを削除する</button>
+      <p v-if="todos.length === 0">ToDoがまだありません！</p>
+      <ul v-else>
+      <!-- 項目追加-->
+    <li v-for="(todo, key) in todos" v-bind:key="key">
+      <input type="checkbox" v-model="todo.isDone" /><span
+        :class="{ 'todo-done': todo.isDone }"
+        >{{ todo.text }}</span
+      >
+    </li>
+   </ul>
+   <!-- ラベル表示 -->
+    <span>{{newTodoText}}が入力されました</span>
+    </div>
+
+    <div class="right-view">
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      newTodoText: '',
+      todos: [],
+    }
+  },
+  methods: {
+     addTodo() {
+      if (!this.newTodoText) return alert('文字を入力してください')
+      this.todos.push({
+        isDone: false,
+        text: this.newTodoText,
+      })
+      this.newTodoText = ''
+    },
+    clearDoneTodos() {
+      this.todos = this.todos.filter((todo) => !todo.isDone)
+    },
+  },
+}
+</script>
+
+<style scoped>
+.main-page {
+  padding: 10px;
+  .left-menu {
+    width: 1400px;
+    background-color: #f7f6f3;
+     .note {
+      margin: 10px 0;
+      display: flex;
+      align-items: center;
+      padding: 5px;
+      color: rgba(25, 23, 17, 0.6);
+    }
+  }
+  .right-view {
+    flex-grow: 1;
+    padding: 10px;
+  }
+}
+</style>
